96557f89b45379f0e8aa5c88718bcee644e65640
00sand <111708646+00sand@users.noreply.github.com>
2022-10-23 18:32:31 -0700

    burger

--- index.js
+++ index.js
@@ -38,24 +38,9 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden')
 hiddenElements.forEach((el) => observer.observe(el))
 
-document.addEventListener('DOMContentLoaded', () => {
+const burgerIcon = document.querySelector("#burger")
+const navBarMenu = document.querySelector("#nav-links")
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-    // Add a click event on each of them
-    $navbarBurgers.forEach(el => {
-        el.addEventListener('click', () => {
-
-            // Get the target from the "data-target" attribute
-            const target = el.dataset.target;
-            const $target = document.getElementById(target);
-
-            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-            el.classList.toggle('is-active');
-            $target.classList.toggle('is-active');
-
-        });
-    });
-
-});
+burgerIcon.addEventListener('click', () => {
+    navBarMenu.classList.toggle('is-active')
+})
